bf75e5c27f7b804d9fe3d23e1efe879c8f3847a6
GagliardeStefano <stefanogagliarde32@gmail.com>
2025-01-10 12:22:01 +0100

    Initial commit - Spring iniziale

---
+++ src/test/java/NC12/LupusInCampus/LupusInCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package NC12.LupusInCampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LupusInCampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
