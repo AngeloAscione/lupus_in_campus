affb65be9088c338ed41d047f65b3d585d7fc542
angelo <goldrakealtaru@gmail.com>
2025-02-17 10:15:20 +0100

    Added queue of messages to devices. When game starts, every player is notified with their role every 2 seconds, until an ACK i received.

--- src/main/java/NC12/LupusInCampus/controller/GameController.java
+++ src/main/java/NC12/LupusInCampus/controller/GameController.java
@@ -5,6 +5,8 @@ import NC12.LupusInCampus.model.Lobby;
 import NC12.LupusInCampus.model.Player;
 import NC12.LupusInCampus.model.dao.GameDAO;
 import NC12.LupusInCampus.model.dao.LobbyDAO;
+import NC12.LupusInCampus.model.dao.PlayerDAO;
+import NC12.LupusInCampus.model.dto.game.GameUpdateMessage;
 import NC12.LupusInCampus.model.enums.ErrorMessages;
 import NC12.LupusInCampus.model.enums.PlayerRole;
 import NC12.LupusInCampus.model.enums.SuccessMessages;
@@ -15,18 +17,17 @@ import NC12.LupusInCampus.service.RequestService;
 import NC12.LupusInCampus.utils.LoggerUtil;
 import NC12.LupusInCampus.utils.Session;
 import NC12.LupusInCampus.utils.clientServerComunication.MessagesResponse;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("controller/game")
@@ -36,18 +37,21 @@ public class GameController {
     private final ListPlayersLobbiesService lobbyLists = new ListPlayersLobbiesService();
     private final GameDAO gameDAO;
     private final MessagesResponse messagesResponse;
+    private final PlayerDAO playerDAO;
 
     @Autowired
-    public GameController(LobbyDAO lobbyDAO, GameDAO gameDAO, MessagesResponse messagesResponse) {
+    public GameController(LobbyDAO lobbyDAO, GameDAO gameDAO, MessagesResponse messagesResponse, PlayerDAO playerDAO) {
         this.lobbyDAO = lobbyDAO;
         this.gameDAO = gameDAO;
         this.messagesResponse = messagesResponse;
+        this.playerDAO = playerDAO;
     }
 
-    @GetMapping("/save-lobby-game-state")
-    public ResponseEntity<?> saveLobbyGameState(@RequestParam String codeLobby, HttpSession session, HttpServletRequest request) {
+    @PostMapping("/start-game")
+    public ResponseEntity<?> saveLobbyGameState(@RequestBody Map<String, String> params, HttpSession session, HttpServletRequest request) {
 
         String endpoint = RequestService.getEndpoint(request);
+        String codeLobby = params.get("codeLobby");
 
         if (!Session.sessionIsActive(session))
             return messagesResponse.createResponse(endpoint, ErrorMessages.PLAYER_NOT_IN_SESSION);
@@ -72,13 +76,22 @@ public class GameController {
         game.setGameDate(LocalDateTime.now());
         gameDAO.save(game);
 
-        // save participants
+        //NOTIFY ALL PLAYERS GAME IS STARTING
+        //WebSocketGameController.notifyGameUpdate(new GameUpdateMessage("GAME_STARTED", "", String.valueOf(lobby.getCode())));
+
+        Player me = playerDAO.findPlayerById(Integer.parseInt(params.get("playerID")));
         List<Player> listPlayerWithRole = roleAssignment(listPlayer);
-        for (Player player : listPlayerWithRole) {
+        for (int i = 0; i < listPlayerWithRole.size() ; i++) {
+            Player player = listPlayerWithRole.get(i);
             gameDAO.saveParticipants(game.getId(), player.getId(), player.getRole());
+            if (player.getId() == Integer.parseInt(params.get("playerID"))) {
+                me = player;
+            } else {
+                WebSocketGameController.addToQueue(player.getNickname(), new GameUpdateMessage("ROLE", player.getRole(), String.valueOf(lobby.getCode())));
+            }
         }
 
-        return messagesResponse.createResponse(endpoint, SuccessMessages.LOBBY_GAME_STATUS_SAVED);
+        return messagesResponse.createResponse(endpoint, SuccessMessages.LOBBY_GAME_STATUS_SAVED, me);
     }
 
     public List<Player> roleAssignment(List<Player> players) {
--- src/main/java/NC12/LupusInCampus/controller/WebSocketGameController.java
+++ src/main/java/NC12/LupusInCampus/controller/WebSocketGameController.java
@@ -1,22 +1,31 @@
 package NC12.LupusInCampus.controller;
 
-import NC12.LupusInCampus.model.dto.game.GameActionDTO;
+import NC12.LupusInCampus.model.dto.game.GameUpdateMessage;
 import NC12.LupusInCampus.model.dto.lobby.LobbyUpdate;
 import NC12.LupusInCampus.model.dto.game.ChatMessage;
+import NC12.LupusInCampus.utils.LoggerUtil;
+import com.google.gson.JsonObject;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.*;
 
 @Controller
 public class WebSocketGameController {
             
-    private final SimpMessagingTemplate messagingTemplate;
+    private static SimpMessagingTemplate messagingTemplate = null;
 
-    WebSocketGameController(SimpMessagingTemplate messagingTemplate) {
-        this.messagingTemplate = messagingTemplate;
+    private static MessageQueueManager messageQueueManager = new MessageQueueManager();
+
+    WebSocketGameController(SimpMessagingTemplate template) {
+        messagingTemplate = template;
     }
 
     // Handle messages sent to a specific lobby chat
@@ -31,6 +40,70 @@ public class WebSocketGameController {
     public void sendLobbyUpdate(@DestinationVariable String lobbyCode, LobbyUpdate update) {
         String topic = "/topic/lobby/" + lobbyCode; // Dynamic topic
         messagingTemplate.convertAndSend(topic, update);
+    }
+
+    public static void assignRolesToPlayers(String nickname, GameUpdateMessage message) {
+        String topic = "/topic/lobby/" + message.getLobbyCode() + "/" + nickname; // Dynamic topic
+        messagingTemplate.convertAndSend(topic, message);
+    }
+
+    public static void notifyGameUpdate(GameUpdateMessage message) {
+        String lobbyCode = message.getLobbyCode();
+        messagingTemplate.convertAndSend("/topic/lobby/"+lobbyCode, message);
+    }
+
+
+    public static void addToQueue(String playerName, GameUpdateMessage message) {
+        messageQueueManager.sendMessageWithRetry(playerName, message);
+    }
+
+    @MessageMapping("/ack")
+    public void receiveAcknowledgment(String nickname) {
+        LoggerUtil.logInfo("Received acknowledgment from " + nickname);
+        messageQueueManager.acknowledgeMessage(nickname);
+    }
+
+
+    public static class MessageQueueManager {
+
+        // Map to track messages waiting for acknowledgment
+        private final Map<String, GameUpdateMessage> pendingMessages = new ConcurrentHashMap<>();
+        // Executor for retrying messages
+        private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+        public void sendMessageWithRetry(String nickname, GameUpdateMessage message) {
+            pendingMessages.putIfAbsent(nickname, message);
+
+            sendNextMessage(nickname);
+        }
+
+        private void sendNextMessage(String playerName) {
+            GameUpdateMessage message = pendingMessages.get(playerName);
+            if (message == null) return;
+
+            LoggerUtil.logInfo("Sending next message to lobby " + message.getLobbyCode() + " for " + playerName);
+
+            assignRolesToPlayers(playerName, message);
+
+            // Schedule a retry if no acknowledgment is received
+            scheduler.schedule(() -> checkAcknowledgment(playerName), 2, TimeUnit.SECONDS);
+        }
+
+        public void acknowledgeMessage(String playerName) {
+
+            JsonObject obj = new JsonObject();
+
+            pendingMessages.remove(playerName);// Remove the acknowledged message
+            LoggerUtil.logInfo("Acknowledging next message to lobby for " + playerName);
+        }
+
+        private void checkAcknowledgment(String playerName) {
+            GameUpdateMessage message = pendingMessages.get(playerName);
+            if (message != null) {
+                LoggerUtil.logInfo("Sending the message again for" + playerName);
+                sendNextMessage(playerName);
+            }
+        }
     }
 
 }
